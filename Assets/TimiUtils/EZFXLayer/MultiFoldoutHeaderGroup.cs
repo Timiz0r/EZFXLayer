49046be7a100f03353d10cddb668dac322d79178
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2022-01-21 21:07:52 -0800

    Selection of blend shapes no longer requires selecting an avatar
    Instead, we'll show all avatars in the popup

---
+++ Assets/TimiUtils/EZFXLayer/MultiFoldoutHeaderGroup.cs
@@ -0,0 +1,22 @@
+﻿namespace TimiUtils.EZFXLayer
+{
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using UnityEditor;
+    using UnityEngine;
+    using VRC.SDK3.Avatars.Components;
+
+    public class MultiFoldoutHeaderGroup
+    {
+        private readonly Dictionary<object, bool> foldoutStatus = new Dictionary<object, bool>();
+
+        public bool Begin(object key, string content, bool defaultFoldoutStatus = true)
+            => foldoutStatus[key] = EditorGUILayout.BeginFoldoutHeaderGroup(
+                foldoutStatus.TryGetValue(key, out var currentFoldoutStatus)
+                    ? currentFoldoutStatus
+                    : defaultFoldoutStatus,
+                content);
+        public void End() => EditorGUILayout.EndFoldoutHeaderGroup();
+    }
+}
