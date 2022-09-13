f1ea12c6b9277e14668f67fa7472b93461bd5742
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2022-09-13 00:28:33 -0700

    Began working on hinding unchanged stuff, but want to pivot to sorting
    Since that'll drive a lot of the design perhaps

--- Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Editor/Inspector/Controls/AnimatableBlendShapeField.cs
+++ Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Editor/Inspector/Controls/AnimatableBlendShapeField.cs
@@ -9,6 +9,8 @@ namespace EZFXLayer.UIElements
     public class AnimatableBlendShapeField : BindableElement, ISerializedPropertyContainerItem
     {
         private AnimatableBlendShape blendShape;
+        private readonly AnimatorLayerComponentEditor editor;
+        private bool hideIfMatchingReference = false;
 
         public AnimatableBlendShapeField(AnimatorLayerComponentEditor editor)
         {
@@ -17,12 +19,28 @@ namespace EZFXLayer.UIElements
             visualTree.CloneTree(this);
 
             this.Q<UnityEngine.UIElements.Button>().clicked += () => editor.RemoveBlendShape(blendShape);
+            this.editor = editor;
         }
 
         public void Rebind(SerializedProperty serializedProperty)
         {
             this.BindProperty(serializedProperty);
             blendShape = Deserialize(serializedProperty);
+        }
+
+        public void SetHideUnchangedItems(bool enabled)
+        {
+            //TODO: to be called when value in animation changes by looping thru all elements
+            hideIfMatchingReference = enabled;
+            //TODO: move this to animation. will be part of selector
+            this.EnableInClassList("hide-unchanged-items", enabled);
+            ApplyMatchingReferenceCheck();
+        }
+
+        private void ApplyMatchingReferenceCheck()
+        {
+            //TODO: to be called when toggle or floatfield are changed. idk if we need two event thingies, prob do
+            //TODO: to be called reference (so this class) -> editor -> ApplyMatchingReferenceCheck
         }
 
         public static AnimatableBlendShape Deserialize(SerializedProperty serializedProperty)
