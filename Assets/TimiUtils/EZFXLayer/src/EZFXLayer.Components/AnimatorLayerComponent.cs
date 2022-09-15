cd24d58c061a0f82a45096525ba29879f799d7bf
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2022-09-15 03:31:49 -0700

    Generate a unique name for new layer components

--- Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Components/AnimatorLayerComponent.cs
+++ Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Components/AnimatorLayerComponent.cs
@@ -8,8 +8,6 @@ namespace EZFXLayer
     [AddComponentMenu("EZFXLayer/EZFXLayer animator layer configuration")]
     public class AnimatorLayerComponent : MonoBehaviour
     {
-        //TODO: verify that shadowing isnt a problem.
-        //want to try it here because it's confusing having an unusable name field
         public new string name;
 
         public AnimationConfiguration referenceAnimation = new AnimationConfiguration()
@@ -20,7 +18,6 @@ namespace EZFXLayer
         };
         public List<AnimationConfiguration> animations = new List<AnimationConfiguration>();
 
-        //field name to be layerName
         public bool manageAnimatorControllerStates = true;
         public bool manageExpressionMenuAndParameters = true;
         //no reasonable scenario in which we won't want to generate animations (for non-empty AnimationConfiguration)
@@ -29,27 +26,11 @@ namespace EZFXLayer
         //  clips in a separate layer using the same parameters
         //public bool manageAnimations = true;
         public string menuPath = null;
-        //useful for viewing, in some cases
         public bool hideUnchangedItemsInAnimationConfigurations = false;
 
-        // private void OnValidate()
-        // {
-        //     if (referenceAnimation == null)
-        //     {
-        //         referenceAnimation = ScriptableObject.CreateInstance<AnimationConfiguration>();
-        //         referenceAnimation.name = "Default";
-        //         //could check for other animations, but this should only happen on first creation
-        //         //since we don't allow unsetting it in the editor
-        //         referenceAnimation.isDefaultAnimation = true;
-        //         //wont add editor support to change this
-        //         referenceAnimation.isDefaultState = true;
-        //     }
-        // }
-        //         public AnimationConfiguration referenceAnimation = new AnimationConfiguration()
-        // {
-        //     name = "Default",
-        //     isDefaultAnimation = true,
-        //     isDefaultState = true //wont add editor support to change this
-        // };
+        //for the initial value of the component
+        //but we dont exclusively go with it because we dont disallow multiple components per GameObject
+        public void Reset() => name = name ?? this.gameObject.name;
+
     }
 }
