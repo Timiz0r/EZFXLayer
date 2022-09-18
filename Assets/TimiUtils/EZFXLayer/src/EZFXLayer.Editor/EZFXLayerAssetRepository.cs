cfcb1ed91a76a919e23ca6012c514efff163c7c0
Timiz0r <5377118+Timiz0r@users.noreply.github.com>
2022-09-17 20:03:36 -0700

    Expressions parameters and menu asset management works great!
    Pre-existing submenus were a slightly speedbump but nothing major. Even if we ultimately don't modify pre-existing submenus, we'll have our own copy anyway. Since the overall design is that the only way changes get in is via regen, this is perfect anyway.

--- Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Editor/EZFXLayerAssetRepository.cs
+++ Assets/TimiUtils/EZFXLayer/src/EZFXLayer.Editor/EZFXLayerAssetRepository.cs
@@ -16,14 +16,15 @@ namespace EZFXLayer
         private readonly string generatedPath;
 
         private readonly AnimatorController referenceController;
-        private readonly VRCExpressionsMenu referenceMenu;
+        private readonly VRCExpressionsMenu referenceRootMenu;
         private readonly VRCExpressionParameters referenceParameters;
         private readonly List<GeneratedClip> generatedClips = new List<GeneratedClip>();
         private readonly List<GeneratedMenu> generatedMenus = new List<GeneratedMenu>();
         private readonly List<UnityEngine.Object> generatedControllerSubassets = new List<UnityEngine.Object>();
 
         private AnimatorController workingController;
-        private VRCExpressionsMenu workingMenu;
+        private readonly List<(VRCExpressionsMenu referenceMenu, VRCExpressionsMenu workingMenu)> workingMenus =
+            new List<(VRCExpressionsMenu referenceMenu, VRCExpressionsMenu workingMenu)>();
         private VRCExpressionParameters workingParameters;
 
         public EZFXLayerAssetRepository(
@@ -34,7 +35,7 @@ namespace EZFXLayer
         {
             generatedPath = Path.Combine(outputPath, "generated");
             this.referenceController = referenceController;
-            this.referenceMenu = referenceMenu;
+            referenceRootMenu = referenceMenu;
             this.referenceParameters = referenceParameters;
         }
 
@@ -44,19 +45,39 @@ namespace EZFXLayer
 
             workingController = new AnimatorController();
             EditorUtility.CopySerialized(referenceController, workingController);
-            workingMenu = ScriptableObject.CreateInstance<VRCExpressionsMenu>();
-            EditorUtility.CopySerialized(referenceMenu, workingMenu);
+
+            VRCExpressionsMenu workingRootMenu = RecordPreExistingMenu(referenceRootMenu);
+
             workingParameters = ScriptableObject.CreateInstance<VRCExpressionParameters>();
             EditorUtility.CopySerialized(referenceParameters, workingParameters);
+
 
             return
             (
                 workingController,
-                workingMenu,
+                workingRootMenu,
                 workingParameters
             );
-    }
 
+            VRCExpressionsMenu RecordPreExistingMenu(VRCExpressionsMenu referenceMenu)
+            {
+                VRCExpressionsMenu workingMenu = ScriptableObject.CreateInstance<VRCExpressionsMenu>();
+                EditorUtility.CopySerialized(referenceMenu, workingMenu);
+#pragma warning disable IDE0037 //simplify valuetuple names
+                workingMenus.Add((referenceMenu: referenceMenu, workingMenu: workingMenu));
+#pragma warning restore IDE0037
+
+                foreach (VRCExpressionsMenu.Control control in workingMenu.controls
+                    .Where(c => c.type == VRCExpressionsMenu.Control.ControlType.SubMenu))
+                {
+                    control.subMenu = RecordPreExistingMenu(control.subMenu);
+                }
+
+                return workingMenu;
+            }
+        }
+
+        //TODO: do a quick test and see if we can delete everything but still get the behavior we ewant
         //there are two goals with regards to how we do this: performance/time and git
         //the most difficult one is the animator controller, since it has many sub assets
         //currently, we mostly give up on this
@@ -67,12 +88,20 @@ namespace EZFXLayer
             try
             {
                 AnimatorController generatedController =
-                    SwapOldGeneratedAssetWithWorkingAsset(workingController, referenceController);
+                    ReplaceOldGeneratedAssetWithWorkingAsset(workingController, referenceController);
                 foreach (UnityEngine.Object subAsset in generatedControllerSubassets)
                 {
                     AssetDatabase.AddObjectToAsset(subAsset, generatedController);
                 }
-                return (generatedController, null, null);
+
+                VRCExpressionsMenu generatedMenu = FinalizeMenus();
+
+                VRCExpressionParameters generatedParameters =
+                    ReplaceOldGeneratedAssetWithWorkingAsset(workingParameters, referenceParameters);
+
+                FinalizeAnimations();
+
+                return (generatedController, generatedMenu, generatedParameters);
             }
             finally
             {
@@ -81,16 +110,63 @@ namespace EZFXLayer
             }
         }
 
-        private void RewriteAnimations() => throw new NotImplementedException();
+        private VRCExpressionsMenu FinalizeMenus()
+        {
+            foreach (GeneratedMenu subMenu in generatedMenus)
+            {
+                SaveGeneratedSubMenu(subMenu);
+            }
 
+            VRCExpressionsMenu generatedRootMenu = null;
+            foreach ((VRCExpressionsMenu referenceMenu, VRCExpressionsMenu workingMenu) menuPair in workingMenus)
+            {
+                VRCExpressionsMenu currentGeneratedMenu =
+                    ReplaceOldGeneratedAssetWithWorkingAsset(menuPair.workingMenu, menuPair.referenceMenu);
+                if (generatedRootMenu == null)
+                {
+                    generatedRootMenu = currentGeneratedMenu;
+                }
+            }
+
+            return generatedRootMenu;
+
+            void SaveGeneratedSubMenu(GeneratedMenu subMenu)
+            {
+                string subMenuFolder = Path.Combine(generatedPath, "GeneratedSubMenus");
+                foreach (string pathComponent in subMenu.PathComponents)
+                {
+                    //we create folders mainly to avoid unlikely but possible name collisions
+                    //such as a path=foo_bar and path=foo/bar couple of submenus
+                    subMenuFolder = Path.Combine(
+                        subMenuFolder,
+                        Regex.Replace(
+                            pathComponent,
+                            $"[{Regex.Escape(new string(Path.GetInvalidPathChars()))}]", "_"));
+                }
+                _ = EnsureFolderCreated(subMenuFolder);
+
+                string subMenuFullPath = Path.Combine(
+                    subMenuFolder,
+                    Regex.Replace(
+                        $"{subMenu.PathComponents[subMenu.PathComponents.Count - 1]}.asset",
+                        $"[{Regex.Escape(new string(Path.GetInvalidFileNameChars()))}]", "_"));
+                _ = ReplaceOldGeneratedAssetWithWorkingAsset(subMenu.Menu, subMenuFullPath);
+            }
+        }
+
+        private void FinalizeAnimations() { }
         //our guid rewriting is easier, since we only care about the meta file
         //as there is no cross-referencing between assets for which this method is invoked
-        private T SwapOldGeneratedAssetWithWorkingAsset<T>(T asset, T referenceAsset) where T : UnityEngine.Object
+        private T ReplaceOldGeneratedAssetWithWorkingAsset<T>(T asset, T referenceAsset) where T : UnityEngine.Object
         {
             string generatedAssetPath = Path.Combine(
                 generatedPath,
                 $"EZFXLayer_{Path.GetFileName(AssetDatabase.GetAssetPath(referenceAsset))}");
 
+            return ReplaceOldGeneratedAssetWithWorkingAsset(asset, generatedAssetPath);
+        }
+        private static T ReplaceOldGeneratedAssetWithWorkingAsset<T>(T asset, string generatedAssetPath) where T : UnityEngine.Object
+        {
             T generatedAsset = AssetDatabase.LoadAssetAtPath<T>(generatedAssetPath);
             if (generatedAsset != null)
             {
@@ -132,9 +208,7 @@ namespace EZFXLayer
         void IAssetRepository.VRCSubMenuAdded(GeneratedMenu menu) => generatedMenus.Add(menu);
         void IAssetRepository.FXAnimatorControllerStateAdded(AnimatorState animatorState)
             => generatedControllerSubassets.Add(animatorState);
-        //no need to remove anything, since the working controller is in-memory, and we add subassets at the end
-        //TODO: verify this is actually to, because it's probably not
-        //though it's really not a big deal to leave what will likely be few subassets around
+        //in testing, the subasset disappears one way or another, so dont care about this
         void IAssetRepository.FXAnimatorControllerStateRemoved(AnimatorState animatorState) { }
         void IAssetRepository.FXAnimatorStateMachineAdded(AnimatorStateMachine stateMachine)
             => generatedControllerSubassets.Add(stateMachine);
